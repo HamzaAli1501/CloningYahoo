c16e4ccb18244831748120ecea0c964a940c5219
HamzaAli1501 <99950533+HamzaAli1501@users.noreply.github.com>
2022-03-26 17:30:12 +0500

    Add files via upload

---
+++ Yahoo/SignIn.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Yahoo
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignIn));
+            this.signin_lbl = new System.Windows.Forms.Label();
+            this.ext_lbl = new System.Windows.Forms.Label();
+            this.pswd_inp = new System.Windows.Forms.MaskedTextBox();
+            this.usern_email_inp = new System.Windows.Forms.MaskedTextBox();
+            this.StaySignedin_check = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.forgot_usrname = new System.Windows.Forms.Label();
+            this.yahoologo = new System.Windows.Forms.PictureBox();
+            this.usernamepanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.googlebtn = new ePOSOne.btnProduct.Button_WOC();
+            this.createbtn = new ePOSOne.btnProduct.Button_WOC();
+            this.signinbtn = new ePOSOne.btnProduct.Button_WOC();
+            ((System.ComponentModel.ISupportInitialize)(this.yahoologo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // signin_lbl
+            // 
+            this.signin_lbl.AutoSize = true;
+            this.signin_lbl.BackColor = System.Drawing.Color.White;
+            this.signin_lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signin_lbl.ForeColor = System.Drawing.Color.Black;
+            this.signin_lbl.Location = new System.Drawing.Point(133, 96);
+            this.signin_lbl.Name = "signin_lbl";
+            this.signin_lbl.Size = new System.Drawing.Size(60, 19);
+            this.signin_lbl.TabIndex = 1;
+            this.signin_lbl.Text = "Sign in";
+            // 
+            // ext_lbl
+            // 
+            this.ext_lbl.AutoSize = true;
+            this.ext_lbl.BackColor = System.Drawing.Color.White;
+            this.ext_lbl.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ext_lbl.ForeColor = System.Drawing.Color.Black;
+            this.ext_lbl.Location = new System.Drawing.Point(66, 115);
+            this.ext_lbl.Name = "ext_lbl";
+            this.ext_lbl.Size = new System.Drawing.Size(215, 21);
+            this.ext_lbl.TabIndex = 6;
+            this.ext_lbl.Text = "using your Yahoo account";
+            // 
+            // pswd_inp
+            // 
+            this.pswd_inp.BackColor = System.Drawing.Color.White;
+            this.pswd_inp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.pswd_inp.Font = new System.Drawing.Font("Century Gothic", 11F);
+            this.pswd_inp.ForeColor = System.Drawing.Color.Black;
+            this.pswd_inp.Location = new System.Drawing.Point(25, 211);
+            this.pswd_inp.Name = "pswd_inp";
+            this.pswd_inp.Size = new System.Drawing.Size(275, 18);
+            this.pswd_inp.TabIndex = 8;
+            this.pswd_inp.MouseEnter += new System.EventHandler(this.pswd_inp_MouseEnter);
+            this.pswd_inp.MouseLeave += new System.EventHandler(this.pswd_inp_MouseLeave);
+            this.pswd_inp.Validating += new System.ComponentModel.CancelEventHandler(this.pswd_inp_Validating);
+            // 
+            // usern_email_inp
+            // 
+            this.usern_email_inp.BackColor = System.Drawing.Color.White;
+            this.usern_email_inp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usern_email_inp.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usern_email_inp.Font = new System.Drawing.Font("Century Gothic", 11F);
+            this.usern_email_inp.ForeColor = System.Drawing.Color.Black;
+            this.usern_email_inp.Location = new System.Drawing.Point(25, 170);
+            this.usern_email_inp.Name = "usern_email_inp";
+            this.usern_email_inp.Size = new System.Drawing.Size(275, 18);
+            this.usern_email_inp.TabIndex = 9;
+            this.usern_email_inp.MouseEnter += new System.EventHandler(this.usern_email_inp_MouseEnter);
+            this.usern_email_inp.MouseLeave += new System.EventHandler(this.usern_email_inp_MouseLeave);
+            this.usern_email_inp.Validating += new System.ComponentModel.CancelEventHandler(this.usern_email_inp_Validating);
+            // 
+            // StaySignedin_check
+            // 
+            this.StaySignedin_check.AutoSize = true;
+            this.StaySignedin_check.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StaySignedin_check.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
+            this.StaySignedin_check.Location = new System.Drawing.Point(12, 257);
+            this.StaySignedin_check.Name = "StaySignedin_check";
+            this.StaySignedin_check.Size = new System.Drawing.Size(126, 24);
+            this.StaySignedin_check.TabIndex = 11;
+            this.StaySignedin_check.Text = "Stay signed in";
+            this.StaySignedin_check.UseVisualStyleBackColor = true;
+            this.StaySignedin_check.CheckedChanged += new System.EventHandler(this.StaySignedin_check_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(101, 424);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Or, continue with";
+            // 
+            // forgot_usrname
+            // 
+            this.forgot_usrname.AutoSize = true;
+            this.forgot_usrname.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forgot_usrname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
+            this.forgot_usrname.Location = new System.Drawing.Point(206, 257);
+            this.forgot_usrname.Name = "forgot_usrname";
+            this.forgot_usrname.Size = new System.Drawing.Size(132, 20);
+            this.forgot_usrname.TabIndex = 16;
+            this.forgot_usrname.Text = "Forgot username";
+            this.forgot_usrname.Click += new System.EventHandler(this.forgot_usrname_Click);
+            // 
+            // yahoologo
+            // 
+            this.yahoologo.Image = ((System.Drawing.Image)(resources.GetObject("yahoologo.Image")));
+            this.yahoologo.Location = new System.Drawing.Point(70, 35);
+            this.yahoologo.Name = "yahoologo";
+            this.yahoologo.Size = new System.Drawing.Size(201, 58);
+            this.yahoologo.TabIndex = 17;
+            this.yahoologo.TabStop = false;
+            // 
+            // usernamepanel
+            // 
+            this.usernamepanel.BackColor = System.Drawing.Color.DimGray;
+            this.usernamepanel.ForeColor = System.Drawing.Color.Black;
+            this.usernamepanel.Location = new System.Drawing.Point(25, 188);
+            this.usernamepanel.Name = "usernamepanel";
+            this.usernamepanel.Size = new System.Drawing.Size(275, 2);
+            this.usernamepanel.TabIndex = 21;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(25, 228);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 2);
+            this.panel1.TabIndex = 22;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(316, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 28);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(94, 455);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 26);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // googlebtn
+            // 
+            this.googlebtn.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            this.googlebtn.ButtonColor = System.Drawing.Color.White;
+            this.googlebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.googlebtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.googlebtn.Location = new System.Drawing.Point(69, 444);
+            this.googlebtn.Name = "googlebtn";
+            this.googlebtn.OnHoverBorderColor = System.Drawing.Color.Transparent;
+            this.googlebtn.OnHoverButtonColor = System.Drawing.Color.White;
+            this.googlebtn.OnHoverTextColor = System.Drawing.Color.DimGray;
+            this.googlebtn.Size = new System.Drawing.Size(201, 48);
+            this.googlebtn.TabIndex = 20;
+            this.googlebtn.Text = "Google";
+            this.googlebtn.TextColor = System.Drawing.Color.DimGray;
+            this.googlebtn.UseVisualStyleBackColor = true;
+            this.googlebtn.Click += new System.EventHandler(this.googlebtn_Click);
+            // 
+            // createbtn
+            // 
+            this.createbtn.BorderColor = System.Drawing.Color.Transparent;
+            this.createbtn.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
+            this.createbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createbtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createbtn.Location = new System.Drawing.Point(70, 360);
+            this.createbtn.Name = "createbtn";
+            this.createbtn.OnHoverBorderColor = System.Drawing.Color.Transparent;
+            this.createbtn.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
+            this.createbtn.OnHoverTextColor = System.Drawing.Color.White;
+            this.createbtn.Size = new System.Drawing.Size(200, 48);
+            this.createbtn.TabIndex = 19;
+            this.createbtn.Text = "Create an account";
+            this.createbtn.TextColor = System.Drawing.Color.White;
+            this.createbtn.UseVisualStyleBackColor = true;
+            this.createbtn.Click += new System.EventHandler(this.createbtn_Click);
+            // 
+            // signinbtn
+            // 
+            this.signinbtn.BorderColor = System.Drawing.Color.Transparent;
+            this.signinbtn.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
+            this.signinbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signinbtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signinbtn.Location = new System.Drawing.Point(70, 303);
+            this.signinbtn.Name = "signinbtn";
+            this.signinbtn.OnHoverBorderColor = System.Drawing.Color.Transparent;
+            this.signinbtn.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
+            this.signinbtn.OnHoverTextColor = System.Drawing.Color.White;
+            this.signinbtn.Size = new System.Drawing.Size(201, 48);
+            this.signinbtn.TabIndex = 18;
+            this.signinbtn.Text = "Sign in";
+            this.signinbtn.TextColor = System.Drawing.Color.White;
+            this.signinbtn.UseVisualStyleBackColor = true;
+            this.signinbtn.Click += new System.EventHandler(this.signinbtn_Click);
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(350, 515);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.usernamepanel);
+            this.Controls.Add(this.googlebtn);
+            this.Controls.Add(this.createbtn);
+            this.Controls.Add(this.signinbtn);
+            this.Controls.Add(this.yahoologo);
+            this.Controls.Add(this.forgot_usrname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.StaySignedin_check);
+            this.Controls.Add(this.usern_email_inp);
+            this.Controls.Add(this.pswd_inp);
+            this.Controls.Add(this.ext_lbl);
+            this.Controls.Add(this.signin_lbl);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SignIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yahoo SignIn";
+            this.Load += new System.EventHandler(this.SignIn_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.yahoologo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label signin_lbl;
+        private System.Windows.Forms.Label ext_lbl;
+        private System.Windows.Forms.MaskedTextBox pswd_inp;
+        private System.Windows.Forms.MaskedTextBox usern_email_inp;
+        private System.Windows.Forms.CheckBox StaySignedin_check;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label forgot_usrname;
+        private System.Windows.Forms.PictureBox yahoologo;
+        private ePOSOne.btnProduct.Button_WOC signinbtn;
+        private ePOSOne.btnProduct.Button_WOC createbtn;
+        private ePOSOne.btnProduct.Button_WOC googlebtn;
+        private System.Windows.Forms.Panel usernamepanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Yahoo/SignIn.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yahoo
+{
+    public partial class SignIn : Form
+    {
+        SqlConnection con;
+
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+        private void SignIn_Load(object sender, EventArgs e)
+        {
+            usern_email_inp.Focus();
+            usern_email_inp.Text = "Username, email or mobile";
+            pswd_inp.Text = "Enter Your Password";
+            usern_email_inp.ForeColor = Color.Gray;
+            pswd_inp.ForeColor = Color.Gray;
+
+            string con_string = ("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\Hamza\\Desktop\\Yahoo\\Yahoo\\YahooLogin.mdf; Integrated Security = True");
+
+            con = new SqlConnection(con_string);
+            if (con.State != System.Data.ConnectionState.Open)
+            {
+                con.Open();
+            }
+        }
+        private void createbtn_Click(object sender, EventArgs e)
+        {
+            SignUp signup = new SignUp();
+            signup.Show();
+            this.Hide();
+        }
+
+        
+
+        private void usern_email_inp_MouseEnter(object sender, EventArgs e)
+        {
+            if (usern_email_inp.Text == "Username, email or mobile")
+            {
+                usern_email_inp.Text = string.Empty;
+            }
+        }
+
+        private void usern_email_inp_MouseLeave(object sender, EventArgs e)
+        {
+            if (usern_email_inp.Text == string.Empty)
+            {
+                usern_email_inp.Text = "Username, email or mobile";
+                usern_email_inp.ForeColor = Color.Gray;
+            }
+        }
+
+        private void pswd_inp_MouseEnter(object sender, EventArgs e)
+        {
+            if (pswd_inp.Text == "Enter Your Password")
+            {
+                pswd_inp.Text = string.Empty;
+            }
+        }
+
+        private void pswd_inp_MouseLeave(object sender, EventArgs e)
+        {
+            if (pswd_inp.Text == string.Empty)
+            {
+                pswd_inp.Text = "Enter Your Password";
+                pswd_inp.ForeColor = Color.Gray;
+            }
+        }
+
+        private void usern_email_inp_Validating(object sender, CancelEventArgs e)
+        {
+            if (usern_email_inp.Text == string.Empty)
+            {
+                errorProvider1.SetError(usern_email_inp, "Please Enter username, email or phone number");
+            }
+        }
+
+        private void pswd_inp_Validating(object sender, CancelEventArgs e)
+        {
+            if (pswd_inp.Text == string.Empty)
+            {
+                errorProvider2.SetError(pswd_inp, "Please Enter password");
+            }
+        }
+
+        private void signinbtn_Click(object sender, EventArgs e)
+        {
+            /*  try
+              {
+                  SqlCommand mycmd = new SqlCommand("SELECT COUNT(*) FROM UserInfo WHERE Email='" + usern_email_inp.Text + "' AND Password='" + pswd_inp.Text + "'", con);
+
+                  mycmd.BeginExecuteNonQuery();
+                  SqlParameter p1 = new SqlParameter("username", usern_email_inp.Text);
+                  SqlParameter p2 = new SqlParameter("password", pswd_inp.Text);
+                  mycmd.Parameters.Add(p1);
+                  mycmd.Parameters.Add(p2);
+                  con.Open();
+                  SqlDataReader rd = mycmd.ExecuteReader();
+                  if (rd.HasRows)
+                  {
+                      rd.Read();
+                      MessageBox.Show("Login successful.");
+                  }
+                  else
+                  {
+                      MessageBox.Show("Invalid username or password.");
+                  }
+
+              }
+              catch (Exception ex)
+              {
+                  MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+              }*/
+            int index = usern_email_inp.Text.IndexOf('@');
+            string eml;
+            if (index == -1)
+            {
+                eml = usern_email_inp.Text + "@yahoo.com";
+            }
+            else { eml = usern_email_inp.Text; }
+
+
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM yahoo WHERE Email='" + eml + "' AND Password='" + pswd_inp.Text + "'", con);
+            DataTable dt = new DataTable(); //this is creating a virtual table  
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+
+                MessageBox.Show("Sign in successful");
+            }
+            else
+                MessageBox.Show("Invalid email or password. Kindly re-try");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            this.Close();
+        }
+
+        private void googlebtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Just for replica", "Blah Blah");
+        }
+
+        private void forgot_usrname_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Doesn't do anything", "Useless");
+        }
+
+        private void StaySignedin_check_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Just for replica", "Blah Blah");
+        }
+    }
+}
